8c6fe5cc2168dd7baba2bfe58deac4a4edb82940
Linh Nguyen <wickedbadassxx@gmail.com>
2023-05-10 10:45:56 -0400

    finishing up writing user input

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
 
 
 def main():
@@ -39,7 +42,16 @@ def main():
         if user_question:
             docs = knowledge_base.similarity_search(user_question)
             st.write(docs)
-        # stop here
+
+            llm = OpenAI()
+            chain = load_qa_chain(llm, chain_type="stuff")
+            with get_openai_callback() as cb:
+
+                response = chain.run(input_documents=docs,
+                                     question=user_question)
+                print(cb)
+
+            st.write(response)
 
 
 if __name__ == '__main__':
